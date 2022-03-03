2d82f757bc4c79803a159656f4cbe47a92a9caca
atwooddc <david.c.atwood@vanderbilt.edu>
2022-03-03 18:27:15 +0100

    just added request_vaccination_id method

---
+++ src/main/python/uc3m_care/vaccine_manager.py
@@ -0,0 +1,19 @@
+"""Module """
+
+class VaccineManager:
+    """Class for providing the methods for managing the vaccination process"""
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def validate_guid(patient_id):
+        """RETURN TRUE IF THE GUID v4 IS RIGHT, OR FALSE IN OTHER CASE"""
+        return True
+
+    def request_vaccination_id(self,
+                               patient_id,
+                               regsitration_type,
+                               name_surname,
+                               phone_number,
+                               age):
+        pass
---
+++ src/unittest/python/test_request_vaccine_tests.py
@@ -0,0 +1,12 @@
+import unittest
+from uc3m_care import VaccineManager
+
+class MyTestCase(unittest.TestCase):
+    def test_something( self ):
+        # execute sth to be tested
+        # check result w assertion
+        self.assertEqual(True, True)
+
+
+if __name__ == '__main__':
+    unittest.main()
